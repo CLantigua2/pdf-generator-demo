4e7630618654dbadfaba5a56573bf9199dc14f50
Carlos Lantigua <clantigua954@gmail.com>
2020-02-17 09:31:30 -0500

    added heroku script and build code to proj

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const server = express();
 const pdfRoute = require("./pdfRoute");
 const logger = require("morgan");
 const cors = require("cors");
+const path = require("path");
 
 server.use(logger("dev"));
 server.use(express.json());
@@ -15,6 +16,14 @@ server.use("/api", pdfRoute);
 server.get("/", (req, res) => {
   res.send("it works");
 });
+
+if (process.env.NODE_ENV === "production") {
+  server.use(express.static("client/build"));
+  server.get("/*", (req, res) => {
+    const index = path.join(__dirname, "client", "build", "index.html");
+    res.sendFile(index);
+  });
+}
 
 server.listen(port, (req, res) => {
   console.log(`listening on port ${port}`);
